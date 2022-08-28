878baf047a60eb7bcf84f2d6a211cd515a2a5b34
amanda <amandassa504@gmail.com>
2022-08-27 23:01:32 -0300

    Exemplos de ataques

---
+++ simulacao/dicionario.js
@@ -0,0 +1,27 @@
+import { createHash } from 'crypto';
+
+class Usuario{
+    constructor(nome, senha){
+        this.nome = nome;
+        this.hash = this.criaHash(senha);
+    }
+    criaHash (senha) {
+        return createHash('sha256').update(senha).digest('hex');
+    }
+    autentica(nome, senha){
+        if (nome === this.nome && this.hash === this.criaHash(senha)){
+            console.log("Usuário autenticado com sucesso")
+            return true;
+        }
+        // console.log("Usuário ou senha incorretos.")
+        return false;
+    }
+}
+const user = new Usuario('joao', 'senha123');
+
+const senhasComuns = ["senha", "123456", "senha123", "admin", "blink182","meuAniversario", "senha123456", "brasil", "102030"];
+
+senhasComuns.map(senha => {
+    if (user.autentica('joao', senha))
+        console.log(`A senha do usuário é ${senha}`);
+});
---
+++ simulacao/forcaBruta.js
@@ -0,0 +1,29 @@
+import { createHash } from 'crypto'
+
+class Usuario{
+    constructor(nome, senha){
+        this.nome = nome;
+        this.hash = this.criaHash(senha);
+    }
+
+    criaHash(senha){
+        return createHash('sha256').update(senha).digest('hex')
+    }
+
+    autentica(nome, senha){
+        if (nome === this.nome && this.hash === this.criaHash(senha)){
+            console.log("Usuário autenticado com sucesso!");
+            return true;
+        }
+
+        //console.log("Usuário ou senha incorretos.");
+        return false;
+    }
+}
+const usuario = new Usuario('joao', '1337')
+
+for (let senhaTeste = 0; senhaTeste < 10000; senhaTeste++){
+    if (usuario.autentica("joao", senhaTeste.toString())){
+        console.log(`A senha do usuário é ${senhaTeste}`)
+    }
+}
